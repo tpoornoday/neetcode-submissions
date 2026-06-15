92271c3b1795806312138bbcd53b965dd778d894
tpoornoday <130460341+tpoornoday@users.noreply.github.com>
2026-06-16 01:03:59 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] arr = new int[2* nums.length];
+
+         for(int i = 0; i < nums.length ; i++)
+        {
+            arr[i] = nums[i]; 
+        }
+
+        for(int i = 0; i < nums.length ; i++)
+        {
+            arr[nums.length + i] = nums[i]; 
+        }
+    return arr;
+    }
+}
